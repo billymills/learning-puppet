96a3b4f8ffb8ff70fe454816420db4c78bfb1143
root <root@learn.localdomain>
2013-01-24 19:14:21 +0000

    initial commit

---
+++ paramclass1.pp
@@ -0,0 +1,7 @@
+# ~/Learning-manifests/paramclass1.pp
+class {'paramclassexample':
+  value1 => 'Something',
+  value2 => 'Something else',
+}
+
+
---
+++ paramclass2.pp
@@ -0,0 +1,5 @@
+# ~Learning-manifests/paramclass2.pp
+class {'paramclassexample':
+  value1 => 'Something',
+}
+
